c7233eea0972efe1637c2034d8ec4a2cadeca968
Stephanie Phillips <steph@ladymacbeth.uk>
2018-11-05 10:35:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file
+function findMatching(d, s) {
+  return d.filter(z=>z==s);
+}
+
+function fuzzyMatch(d, s) {
+  console.log(d.localeCompare(s, 'en', {sensitivity: 'base'}));
+}
